97cd767bf4288fae15f2d8bc775015dfc444cda7
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-29 11:53:31 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -36,8 +36,11 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, lang_info|
     lang_info.each do |name, attributes|
-      if new_hash.has_key
-      new_hash[name] = {:type => attributes[:type], :style => [style]}
+      if new_hash.has_key?(name)
+        new_hash[name][:style].push(style)
+      else
+        new_hash[name] = {:type => attributes[:type], :style => [style]}
+      end
     end
   end
   new_hash
